270cb65de661e3a8b7df7a92a895d4c03a7e5392
DaProclaima <sn.itpro@gmail.com>
2020-01-14 13:28:01 +0100

    tests: all tests working except case 6

--- api/model/todoItem.js
+++ api/model/todoItem.js
@@ -2,11 +2,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TodoItemSchema = new Schema({
-    name: {type: String, required: true},
-    status: {type: String, enum: ['todo', 'inProgress', 'done'], default: 'todo', required: true}
+const TodoItemSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String, enum: [
+        'todo',
+        'inProgress',
+        'done'
+      ],
+      default: 'todo',
+      required: true
+    }
   },
-  {timestamps: true}
+  {
+    timestamps: true
+  }
 );
 
 module.exports = TodoItemSchema;
--- test/test.js
+++ test/test.js
@@ -1,11 +1,110 @@
 var chai = require('chai')
   , chaiHttp = require('chai-http');
+// import server from '../index';
+
 
 chai.use(chaiHttp);
+var expect = chai.expect;
 
-chai.request('http://127.0.0.1:3000').send({
-  "name":"Ma tache ",
-  "status":"inProgress" }).end(function (err, res) {
-  expect(err).to.be.null;
-  expect(res).to.have.status(200);
-});
+// case 1
+describe('GET REQUEST RETURN A STATUS 404 OK',
+  function () {
+    it('Response should be 404 GET REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').get('/wrongitem')
+          .end(function (err, res) {
+            expect(res).to.have.status(404);
+          });
+      });
+  });
+
+
+
+// case 2
+describe('GET REQUEST RETURN A STATUS 200 OK',
+  function () {
+    it('Response should be 200 OK on a GET REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').get('/')
+          .end(function (err, res) {
+            expect(res).to.have.status(200);
+          });
+      });
+  });
+
+// case 3
+describe('POST REQUEST RETURN A STATUS 200 OK',
+  function () {
+    it('Response should be 200 OK on a POST REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').post('/')
+          .end(function (err, res) {
+            expect(res).to.have.status(200);
+          });
+      });
+  });
+
+// case 3.1
+describe('POST REQUEST RETURN A STATUS 500 wrong body request',
+  function () {
+    it('Response should be 500 wrong body request on a POST REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').post('{\n' +
+          '\t"name":"Ma tache ",\n' +
+          '\t"status":"wrong"\n' +
+          '}')
+          .end(function (err, res) {
+            expect(res).to.have.status(500);
+          });
+      });
+  });
+
+// case 4
+describe('PUT REQUEST RETURN A STATUS 200 OK',
+  function () {
+    it('Response should be 200 OK on a POST REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').put('{\n' +
+          '\t"name":"Ma tache ",\n' +
+          '\t"status":"inProgress"\n' +
+          '}')
+          .end(function (err, res) {
+            expect(res).to.have.status(200);
+          });
+      });
+  });
+
+
+// case 5
+describe('DELETE REQUEST RETURN A STATUS 200 OK',
+  function () {
+    it('Response should be 200 OK on a DELETE REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').put('{\n' +
+          '\t"name":"Ma tache ",\n' +
+          '\t"status":"done"\n' +
+          '}')
+          .end(function (err, res) {
+            expect(res).to.have.status(200);
+          });
+      });
+  });
+
+// case 6
+describe('DELETE REQUEST RETURN A STATUS 200 OK',
+  function () {
+    it('Response should be 200 OK on a DELETE REQUEST',
+      function () {
+        chai.request('http://localhost:3000/todoitems').put('{\n' +
+          '\t"name":"Ma tache ",\n' +
+          '\t"status":"done"\n' +
+          '}')
+          .end(function (err, res) {
+            expect(res).to.have.status(200);
+          });
+      });
+  });
+
+
+
+
